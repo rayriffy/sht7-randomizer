79b4852f55cca0f38f3347e8c1093a3028fc569a
Phumrapee Limpianchop <git@rayriffy.com>
2023-06-25 03:10:39 +0900

    chore: staged changes

--- src/Controller.tsx
+++ src/Controller.tsx
@@ -1,17 +1,22 @@
-import { useStore } from "@nanostores/react"
-import { playersAtom } from "./atoms/playersAtom"
-import { playAtom } from "./atoms/playAtom"
+import { useStore } from '@nanostores/react'
+import { playersAtom } from './atoms/playersAtom'
+import { playAtom } from './atoms/playAtom'
 
 export const Controller = () => {
   const play = useStore(playAtom)
   const players = useStore(playersAtom)
 
-
   return (
     <section className="w-full h-full flex flex-col justify-between">
       <div></div>
       <div className="p-8">
-        <button className="text-2xl text-white bg-pink-500 py-4 w-full rounded-xl font-bold" onClick={() => playAtom.set(!play)}>{play ? 'STOP' : 'START'}</button>
+        <button
+          className={`text-2xl text-white disabled:bg-pink-300 py-4 w-full rounded-xl font-bold ${play ? 'bg-red-500' : 'bg-pink-500'}`}
+          disabled={players.length === 0}
+          onClick={() => playAtom.set(!play)}
+        >
+          {play ? 'STOP' : 'START'}
+        </button>
       </div>
     </section>
   )
